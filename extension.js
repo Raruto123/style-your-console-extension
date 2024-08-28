b9e5e1b7692016008691622d3d68aa790e320431
mohamedsane <sanem9206@gmail.com>
2024-08-28 17:29:28 +0000

    Update the extension
    Second main functionnality done now extra features will be developed

--- extension.js
+++ extension.js
@@ -9,18 +9,35 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
+	//commande pour insérer un console.log() simple stylisé
 	let disposable = vscode.commands.registerCommand("extension.insertStyledVoidConsoleLog", () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const newLine = editor.document.lineAt(5);
-			const snippet = new vscode.SnippetString('\nconsole.log("%cYour text goes here", "Your CSS goes here")');
-			editor.insertSnippet(snippet);
-			vscode.window.showInformationMessage(`${newLine.lineNumber}`);
+			const snippet = new vscode.SnippetString('\nconsole.log("%cYour text goes here", "Your CSS goes here")\n');
+			editor.insertSnippet(snippet, editor.selection.end.with(editor.selection.end.line + 1, 0));
 		}
+	});
+	//commande pour insérer un console.log() stylisé avec une variable
+	let disposableWithVariable = vscode.commands.registerCommand("extension.insertStyledWithVariableConsoleLog", () => {
+		const editor= vscode.window.activeTextEditor;
+		if (editor) {
+			const selection = editor.selection;
+			//To trim the spaces
+			let text = editor.document.getText(selection).trim();
+			if (text) {
+				const insertPosition = selection.end.with(selection.end.line + 1, 0); //Go back to the line
+
+				editor.edit((editBuilder) => {
+					editBuilder.insert(insertPosition, `\nconsole.log(\`%cAny text you want\${${text}}\`, "your css goes here");\n`)
+				}).then(() => {
+					editor.selection = new vscode.Selection(insertPosition, insertPosition);//Place the cursors on a new line
+				})
+			};
+		};
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableWithVariable);
 	
 }
 // This method is called when your extension is deactivated
