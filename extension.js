f13627a7af6174b3ec6ca704be9a3759c96aea80
mohamedsane <sanem9206@gmail.com>
2024-09-02 16:49:37 +0000

    Update the extension
    Almost done it leaves keybindings and some more tweaks

--- extension.js
+++ extension.js
@@ -10,13 +10,26 @@ const vscode = require('vscode');
  */
 function activate(context) {
 	//commande pour insérer un console.log() simple stylisé
-	let disposable = vscode.commands.registerCommand("extension.insertStyledVoidConsoleLog", () => {
+	let disposable = vscode.commands.registerCommand(
+		"extension.insertStyledVoidConsoleLog", () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const snippet = new vscode.SnippetString('\nconsole.log("%cYour text goes here", "Your CSS goes here")\n');
-			// editor.insertSnippet(snippet, editor.selection.end.with(editor.selection.end.line + 1, 0));
-			// editor.insertSnippet(snippet, editor.selection.end);
-			editor.insertSnippet(snippet);
+			const snippet = new vscode.SnippetString(
+				'\nconsole.log("%cYour text goes here", "Your CSS goes here")\n'
+			);
+			const position = editor.selection.active;
+			const document = editor.document;
+	
+			let newPosition;
+	
+			if (position.line === document.lineCount - 1) {
+				// Si le curseur est sur la dernière ligne, on insère le snippet à la fin du document
+				newPosition = position.with(position.line, document.lineAt(position.line).range.end.character);
+			} else {
+				// Sinon, on l'ajoute à la ligne suivante
+				newPosition = position.with(position.line + 1, 0);
+			}
+			editor.insertSnippet(snippet, newPosition);
 		}
 	});
 	//commande pour insérer un console.log() stylisé avec une variable
@@ -38,7 +51,7 @@ function activate(context) {
 		};
 	});
 
-	vscode.commands.registerCommand('extension.commentAllConsoleLogs', function () {
+	let disposableComment = vscode.commands.registerCommand('extension.commentAllConsoleLogs', function () {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const text = editor.document.getText();
@@ -54,12 +67,45 @@ function activate(context) {
 		}
 	});
 
+	let disposableUncomment = vscode.commands.registerCommand('extension.uncommentAllConsoleLogs', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const text = editor.document.getText();
+			const regex = /\/\/\s*(console\.log\(`%cAny text you want.*\);)/g;
+			const uncommentedText = text.replace(regex, "$1");
+	
+			editor.edit(editBuilder => {
+				const firstLine = editor.document.lineAt(0);
+				const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+				const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+				editBuilder.replace(fullRange, uncommentedText);
+			});
+		}
+	});
 
-
+	let disposableDelete = vscode.commands.registerCommand('extension.deleteAllConsoleLogs', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const text = editor.document.getText();
+			const regex = /console\.log\(`%cAny text you want.*\);/g;
+			const deletedText = text.replace(regex, "");
+	
+			editor.edit(editBuilder => {
+				const firstLine = editor.document.lineAt(0);
+				const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+				const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+				editBuilder.replace(fullRange, deletedText);
+			});
+		}
+	});
 
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableWithVariable);
+	context.subscriptions.push(disposableComment);
+	context.subscriptions.push(disposableDelete);
+	context.subscriptions.push(disposableUncomment);
+
 	
 }
 // This method is called when your extension is deactivated
